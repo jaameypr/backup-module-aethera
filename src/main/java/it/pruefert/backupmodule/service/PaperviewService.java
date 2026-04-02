96c0572947ed5738e9ab5e5616bb901ef5ab58bf
Jamal <pruefertjamey@gmail.com>
2026-04-02 20:08:17 +0200

    fix: remove unused WebClient import causing compilation failure

--- src/main/java/it/pruefert/backupmodule/service/PaperviewService.java
+++ src/main/java/it/pruefert/backupmodule/service/PaperviewService.java
@@ -9,7 +9,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.nio.file.Path;
 import java.util.Map;
